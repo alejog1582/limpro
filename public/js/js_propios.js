9d96c3c9c3446670af92d3c4cc65d1efd46ba4de
alejog1582 <alejog1582@gmail.com>
2018-09-12 00:08:07 -0500

    ultimo intento

---
+++ public/js/js_propios.js
@@ -0,0 +1,53 @@
+function mostrar(){
+document.getElementById('carta').style.display = 'block';}
+
+
+//Carousel
+$('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            indicators: true
+        });
+        setInterval(function(){
+            //$('.carousel.carousel-slider').carousel('next');
+        },5000)
+//Fin-Carousel
+//
+//
+
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+      format: 'dd-mm-yyyy',
+        firstDay: 1,
+        yearRange: 100,
+        i18n: ({
+        cancel: "cancelar",
+        months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthsShort: [ 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        weekdays: [ 'Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+        weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+            weekdaysAbbrev:  ['D','L','M','M','J','V','S']
+
+      })
+    });
+  });
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
+
+$(".dropdown-trigger").dropdown();
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
